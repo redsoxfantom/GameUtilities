89ab83ffe64456957f5ff70de34661814c68113e
Tom <redsoxfantom@gmail.com>
2014-12-25 21:07:20 -0500

    Finished adding data contracts

--- GameUtilities/Entities/DataContracts/EntityData.cs
+++ GameUtilities/Entities/DataContracts/EntityData.cs
@@ -17,33 +17,35 @@ namespace GameUtilities.Entities.DataContracts
         public string Name { get; private set; }
 
         /// <summary>
-        /// The Assembly that this Entity will use. If not defined, the assembly defined in the Type will be used
-        /// </summary>
-        [DataMember(IsRequired=false,Order=1)]
-        public string Assembly { get; private set; }
-
-        /// <summary>
         /// The Type of Entity this is. Must be defined and must have the same name as an EntityType config file
         /// </summary>
         [DataMember(IsRequired=true, Order=2)]
         public string Type { get; private set; }
 
         /// <summary>
-        /// The Position the Entity will occupy. If not defined, (0.0,0.0,0.0) is assumed
-        /// </summary>
-        [DataMember(IsRequired=false,Order=3)]
-        public Vector3d Position { get; private set; }
-
-        /// <summary>
-        /// The Scale the entity will have. If not defined, (1.0,1.0,1.0) is assumed
-        /// </summary>
-        [DataMember(IsRequired=false,Order=4)]
-        public Vector3d Scale { get; private set; }
-
-        /// <summary>
         /// A list of optional Data to feed to Components
         /// </summary>
         [DataMember(IsRequired=false,Order=5)]
         public DataSet DataSet { get; private set; }
+
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        /// <param name="type">The Type of entity</param>
+        /// <param name="name">The entity's name</param>
+        /// <param name="dataSet">optional data</param>
+        public EntityData(string type, string name = null, DataSet dataSet = null)
+        {
+            Type = type;
+            Name = name;
+            if(dataSet == null)
+            {
+                DataSet = new DataSet();
+            }
+            else
+            {
+                DataSet = dataSet;
+            }
+        }
     }
 }
--- GameUtilities/Entities/DataContracts/EntityTypeData.cs
+++ GameUtilities/Entities/DataContracts/EntityTypeData.cs
@@ -1,13 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace GameUtilities.Entities.DataContracts
 {
-    class EntityTypeData
+    /// <summary>
+    /// Data contract for each Entity Type
+    /// </summary>
+    [DataContract]
+    public class EntityTypeData
+    {
+        /// <summary>
+        /// Name of the Type. Overrides the Entity name if not defined
+        /// </summary>
+        [DataMember(IsRequired=true,Order = 0)]
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// The assembly the Entity will instantiate
+        /// </summary>
+        [DataMember(IsRequired=true,Order=1)]
+        public string Assembly { get; private set; }
+
+        /// <summary>
+        /// A list of Components associated with a Entity Type
+        /// </summary>
+        [DataMember(IsRequired=false,Order=2)]
+        public ComponentList Components { get; private set; }
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="name">The Entity Type Name</param>
+        /// <param name="assembly">The Entity Assembly</param>
+        /// <param name="components">The Entity's Components</param>
+        public EntityTypeData(string name, string assembly, ComponentList components = null)
+        {
+            Name = name;
+            Assembly = assembly;
+            if(components == null)
+            {
+                Components = new ComponentList();
+            }
+            else
+            {
+                Components = components;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Data contract for a list of Components associated with each Entity type
+    /// </summary>
+    [DataContract]
+    public class ComponentList : List<ComponentEntry>
     {
 
+    }
+
+    /// <summary>
+    /// Data contract representing a single component in the ComponentList
+    /// </summary>
+    [DataContract]
+    public class ComponentEntry
+    {
+        /// <summary>
+        /// The assembly of the Component
+        /// </summary>
+        [DataMember(IsRequired=true,Order=0)]
+        public string Component{get; private set;}
+
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        /// <param name="component">the component's assembly</param>
+        public ComponentEntry(string component)
+        {
+            Component = component;
+        }
     }
 }
