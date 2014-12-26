ffd0d4df3cbe9de68cad19dd25a9273e12ad89ae
Tom <redsoxfantom@gmail.com>
2014-12-25 21:41:12 -0500

    Added a SerializeObject function to datacontractfactory

--- GameUtilities/Framework/DataContracts/DataContractFactory.cs
+++ GameUtilities/Framework/DataContracts/DataContractFactory.cs
@@ -18,15 +18,27 @@ namespace GameUtilities.Framework.DataContracts
         public static T DeserializeObject<T>(string path)
         {
             T obj;
-            FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            FileStream fs = new FileStream(path, FileMode.Open);
             XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
-
-            // Create the DataContractSerializer instance.
             DataContractSerializer ser = new DataContractSerializer(typeof(T));
 
             // Deserialize the data and read it from the instance.
             obj = (T)ser.ReadObject(reader);
+            fs.Close();
+            reader.Close();
             return obj;
+        }
+
+        public static void SerializeObject<T>(string path, T obj)
+        {
+            FileStream fs = new FileStream(path,FileMode.Create);
+            XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs);
+            DataContractSerializer ser = new DataContractSerializer(typeof(T));
+
+            //Serialize the data to a file
+            ser.WriteObject(writer, obj);
+            writer.Close();
+            fs.Close();
         }
     }
 }
