e97c46f309b5c2a707be6843c757f9eb41281021
Tom <redsoxfantom@gmail.com>
2014-12-25 21:26:28 -0500

    Moved config files

--- GameUtilities/Framework/DataContracts/DataSet.cs
+++ GameUtilities/Framework/DataContracts/DataSet.cs
@@ -6,7 +6,7 @@ namespace GameUtilities.Framework.DataContracts
     /// <summary>
     /// A data contract defining a Dictionary of Data Entries, that will be fed into each Component attached to a particular entity
     /// </summary>
-    [DataContract]
+    [CollectionDataContract(KeyName="Key",ValueName="Value", ItemName="Entry")]
     public class DataSet : Dictionary<string,string>
     {}
 }
--- GameUtilitiesRunner/Program.cs
+++ GameUtilitiesRunner/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameUtilities.Framework.DataContracts;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
 
 namespace GameUtilitiesRunner
 {
@@ -10,6 +10,23 @@ namespace GameUtilitiesRunner
     {
         static void Main(string[] args)
         {
+            DataSet set = new DataSet();
+            set.Add("KEY", "VALUE");
+            set.Add("KEY2", "VALUE2");
+
+            WriteObject("TEST.XML", set);
         }
+
+        public static void WriteObject(string path, DataSet set)
+        {
+            FileStream fs = new FileStream(path,FileMode.Create);
+            XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs);
+            DataContractSerializer ser = new DataContractSerializer(typeof(DataSet));
+            ser.WriteObject(writer, set);
+            Console.WriteLine("Finished writing object.");
+            writer.Close();
+            fs.Close();
+        }
+
     }
 }
