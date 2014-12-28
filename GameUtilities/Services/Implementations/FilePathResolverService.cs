b726107ddbd0565708e6349ab9a4e1821db16268
Tom <redsoxfantom@gmail.com>
2014-12-27 21:25:16 -0500

    Added a basic file resolver service.

---
+++ GameUtilities/Services/Implementations/FilePathResolverService.cs
@@ -0,0 +1,56 @@
+﻿using GameUtilities.Services.Interfaces;
+
+namespace GameUtilities.Services.Implementations
+{
+    /// <summary>
+    /// Service to allow components to find file paths
+    /// </summary>
+    public class FilePathResolverService : IFilePathResolverService
+    {
+        #region Fields
+        /// <summary>
+        /// Stores the location of the Root config directory
+        /// </summary>
+        private string mRootDirectory;
+        #endregion Fields
+
+        #region IFilePathResolverService Methods
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public FilePathResolverService()
+        {
+            mRootDirectory = string.Empty;
+        }
+
+        /// <summary>
+        /// Initialize the Service
+        /// </summary>
+        /// <param name="pathToConfigDirectory">path to the /Config/ directory</param>
+        void Init(string pathToConfigDirectory)
+        {
+            mRootDirectory = pathToConfigDirectory;
+        }
+
+        /// <summary>
+        /// Finds the Xml document defining a World
+        /// </summary>
+        /// <param name="worldName">The name of the World to find</param>
+        /// <returns>A string with the complete path to the world</returns>
+        string FindWorld(string worldName)
+        {
+            return mRootDirectory + "\\Worlds\\" + worldName + ".xml";
+        }
+
+        /// <summary>
+        /// Finds the Xml document defining an Entity Type
+        /// </summary>
+        /// <param name="entityTypeName">The name of the entity type to find</param>
+        /// <returns>A string with the complete path to the entity type file</returns>
+        string FindEntityType(string entityTypeName)
+        {
+            return mRootDirectory + "\\EntityTypes\\" + entityTypeName + ".xml";
+        }
+        #endregion IFilePathResolverService Methods
+    }
+}
---
+++ GameUtilities/Services/Interfaces/IFilePathResolverService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameUtilities.Services.Interfaces
+{
+    /// <summary>
+    /// Interface for finding the path to various config files
+    /// </summary>
+    interface IFilePathResolverService
+    {
+        /// <summary>
+        /// Initialize the Service
+        /// </summary>
+        /// <param name="pathToConfigDirectory">path to the /Config/ directory</param>
+        void Init(string pathToConfigDirectory);
+
+        /// <summary>
+        /// Finds the Xml document defining a World
+        /// </summary>
+        /// <param name="worldName">The name of the World to find</param>
+        /// <returns>A string with the complete path to the world</returns>
+        string FindWorld(string worldName);
+
+        /// <summary>
+        /// Finds the Xml document defining an Entity Type
+        /// </summary>
+        /// <param name="entityTypeName">The name of the entity type to find</param>
+        /// <returns>A string with the complete path to the entity type file</returns>
+        string FindEntityType(string entityTypeName);
+    }
+}
