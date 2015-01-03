5fb9c450e253eb38522a5a1f7dd308f49f945af9
Tom <redsoxfantom@gmail.com>
2015-01-02 23:33:56 -0500

    Made gameutilitiesrunner parse command line arguments

---
+++ GameUtilitiesRunner/GameUtilitiesRunnerMain.cs
@@ -0,0 +1,77 @@
+﻿using GameUtilities.Framework.Loggers;
+using System;
+
+namespace GameUtilitiesRunner
+{
+    /// <summary>
+    /// Parses arguments and creates the world
+    /// </summary>
+    class GameUtilitiesRunnerMain
+    {
+        /// <summary>
+        /// The path to the config directory
+        /// </summary>
+        private static string mPath = null;
+
+        /// <summary>
+        /// name of the world to initialize
+        /// </summary>
+        private static string mWorld = null;
+
+        /// <summary>
+        /// Logging level to initialize
+        /// </summary>
+        private static LoggerLevel mLoggingLevel = LoggerLevel.INFO;
+
+        private static string UsageString =
+            "USAGE: GameUtilitiesRunner.exe -loglevel {DEBUG,INFO,WARN,ERROR,FATAL} -world <world name> -path <path\\to\\config\\directory>";
+
+        static void Main(string[] args)
+        {
+            ParseArgs(args);
+
+            GameUtilitiesRunnerProcessor proc = new GameUtilitiesRunnerProcessor(mLoggingLevel, mWorld, mPath);
+            proc.Run();
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Parse out the arguments passed to the program
+        /// </summary>
+        /// <param name="args">the system arguments</param>
+        private static void ParseArgs(string[] args)
+        {
+            if(args.Length != 6)
+            {
+                Console.WriteLine(UsageString);
+                throw new InvalidOperationException("Invalid arguments");
+            }
+
+            for(int i = 0; i < 6; i += 2)
+            {
+                switch(args[i])
+                {
+                    case "-loglevel":
+                        mLoggingLevel = (LoggerLevel)Enum.Parse(typeof(LoggerLevel), args[i + 1]);
+                        break;
+                    case "-world":
+                        mWorld = args[i + 1];
+                        break;
+                    case "-path":
+                        mPath = args[i + 1];
+                        break;
+                    default:
+                        Console.WriteLine(UsageString);
+                        throw new InvalidOperationException(string.Format("Invalid argument: {0}",args[i]));
+                }
+            }
+
+            if(string.IsNullOrEmpty(mWorld)||string.IsNullOrEmpty(mPath))
+            {
+                Console.WriteLine(UsageString);
+                throw new InvalidOperationException("Invalid arguments");
+            }
+        }
+    }
+}
---
+++ GameUtilitiesRunner/GameUtilitiesRunnerProcessor.cs
@@ -0,0 +1,88 @@
+﻿using GameUtilities.Framework.Engine;
+using GameUtilities.Framework.Loggers;
+using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace GameUtilitiesRunner
+{
+    /// <summary>
+    /// Handles actually running the Engine
+    /// </summary>
+    public class GameUtilitiesRunnerProcessor
+    {
+        /// <summary>
+        /// Logging level of the Engine
+        /// </summary>
+        private LoggerLevel mLoggerLevel;
+
+        /// <summary>
+        /// Name of the World to initialize
+        /// </summary>
+        private string mWorld;
+
+        /// <summary>
+        /// Path to the config directory to use
+        /// </summary>
+        private string mPathToConfig;
+
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        /// <param name="loggingLevel">Logging level of the Engine</param>
+        /// <param name="world">Name of the World to load initially</param>
+        /// <param name="PathToConfig">Path to the config directory</param>
+        public GameUtilitiesRunnerProcessor(LoggerLevel loggingLevel, string world, string PathToConfig)
+        {
+            mLoggerLevel = loggingLevel;
+            mWorld = world;
+            mPathToConfig = PathToConfig;
+        }
+
+        /// <summary>
+        /// Run the Engine
+        /// </summary>
+        public void Run()
+        {
+            LoggerFactory.SetLoggingLevel(mLoggerLevel);
+            IEngine engine = new Engine();
+
+            using (var game = new GameWindow())
+            {
+                game.Load += (sender, e) =>
+                {
+                    engine.Init(mPathToConfig, mWorld);
+                    game.VSync = VSyncMode.On;
+                    GL.ClearColor(Color.Black);
+                };
+
+                game.Resize += (sender, e) =>
+                {
+                    GL.Viewport(0, 0, game.Width, game.Height);
+                };
+
+                game.UpdateFrame += (sender, e) =>
+                {
+                    engine.Update(e.Time);
+                    if (game.Keyboard[Key.Escape])
+                    {
+                        engine.Terminate();
+                        game.Exit();
+                    }
+                };
+
+                game.RenderFrame += (sender, e) =>
+                {
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                    engine.Draw(e.Time);
+                    game.SwapBuffers();
+                };
+
+                game.Run();
+            }
+        }
+    }
+}
