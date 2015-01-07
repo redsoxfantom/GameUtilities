0ae3a9f707609890eafd1401dfb93ee358f3c3dd
Tom <redsoxfantom@gmail.com>
2015-01-06 20:11:37 -0500

    Added a Base Service

---
+++ GameUtilities/Services/BaseService.cs
@@ -0,0 +1,54 @@
+﻿using GameUtilities.Framework.Utilities.ExecutableContext;
+using GameUtilities.Framework.Utilities.Loggers;
+
+namespace GameUtilities.Services
+{
+    /// <summary>
+    /// Base class for all Services
+    /// </summary>
+    public class BaseService : IService
+    {
+        /// <summary>
+        /// The service's Logger
+        /// </summary>
+        private ILogger mLogger;
+
+        /// <summary>
+        /// The service's Executable Context
+        /// </summary>
+        private IExecutableContext mContext;
+
+        /// <summary>
+        /// The Service's Name
+        /// </summary>
+        private string mName;
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public BaseService()
+        {
+            mName = this.GetType().ToString();
+            mLogger = LoggerFactory.CreateLogger(mName);
+        }
+
+        /// <summary>
+        /// Initialize the Service
+        /// </summary>
+        /// <param name="context">the service's ExecutableContext</param>
+        public virtual void Init(IExecutableContext context)
+        {
+            mLogger.Info(string.Format("Creating Service {0}",mName));
+            mContext = context;
+        }
+
+        /// <summary>
+        /// Called once a frame
+        /// </summary>
+        /// <param name="timeSinceLastFrame">How long it's been since the last frame</param>
+        public virtual void Update(double timeSinceLastFrame)
+        {
+            //Overriden by child classes
+        }
+    }
+}
---
+++ GameUtilities/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using GameUtilities.Framework;
+using GameUtilities.Framework.Utilities.ExecutableContext;
+
+namespace GameUtilities.Services
+{
+    /// <summary>
+    /// Base interface for a Service
+    /// </summary>
+    public interface IService : IUpdatable
+    {
+        /// <summary>
+        /// Initialize the Service
+        /// </summary>
+        /// <param name="context"></param>
+        void Init(IExecutableContext context);
+    }
+}
