6433a3edabc3316ed09697c77e74d8a6a31da5c8
Tom <redsoxfantom@gmail.com>
2015-01-06 21:09:06 -0500

    Created a base Message

---
+++ GameUtilities/Framework/Utilities/Message/BaseMessage.cs
@@ -0,0 +1,31 @@
+﻿namespace GameUtilities.Framework.Utilities.Message
+{
+    /// <summary>
+    /// The base class for Messages
+    /// </summary>
+    public class BaseMessage : IMessage
+    {
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public BaseMessage()
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize the Message
+        /// </summary>
+        public virtual void Init()
+        { }
+
+        /// <summary>
+        /// Gets the message's data
+        /// </summary>
+        /// <returns>a data object</returns>
+        public virtual object GetData()
+        {
+            return null;
+        }
+    }
+}
---
+++ GameUtilities/Framework/Utilities/Message/IMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace GameUtilities.Framework.Utilities.Message
+{
+    /// <summary>
+    /// The interface for Message objects
+    /// </summary>
+    public interface IMessage
+    {
+        /// <summary>
+        /// Initialize the Message
+        /// </summary>
+        void Init();
+
+        /// <summary>
+        /// Gets the data associated with the message
+        /// </summary>
+        /// <returns>a data object</returns>
+        object GetData();
+    }
+}
