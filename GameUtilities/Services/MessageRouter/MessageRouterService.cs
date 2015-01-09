c6b3a27c5c756c89a6c04a6bf70a86b89c658d96
Tom <redsoxfantom@gmail.com>
2015-01-08 21:26:20 -0500

    Started on the MessageRouter

---
+++ GameUtilities/Services/MessageRouter/MessageRouterService.cs
@@ -0,0 +1,13 @@
+﻿using GameUtilities.Services;
+
+namespace GameUtilities.Services.MessageRouter
+{
+    /// <summary>
+    /// The message router service.
+    /// Uses a subscription-based model where Components and Services can register for Topics, and then retrieve Messages each Frame
+    /// </summary>
+    public class MessageRouterService : IMessageRouterService, BaseService
+    {
+
+    }
+}
