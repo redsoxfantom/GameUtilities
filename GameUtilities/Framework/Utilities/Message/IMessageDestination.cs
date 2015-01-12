82788ccd7068da642468c31d86a8a216a4d2b7ad
Tom <redsoxfantom@gmail.com>
2015-01-11 22:02:09 -0500

    Added an IMessageDestination interface

---
+++ GameUtilities/Framework/Utilities/Message/IMessageDestination.cs
@@ -0,0 +1,16 @@
+﻿namespace GameUtilities.Framework.Utilities.Message
+{
+    /// <summary>
+    /// Interface for any object that can receive a message
+    /// </summary>
+    interface IMessageDestination
+    {
+        /// <summary>
+        /// Handle a message
+        /// </summary>
+        /// <param name="message">The sent message</param>
+        /// <param name="returnValue">An object containing the return value</param>
+        /// <returns>Whether the message was handled or not</returns>
+        bool HandleMessage(IMessage message, ref object returnValue);
+    }
+}
