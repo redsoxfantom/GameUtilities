08436eeea884c394570e70227c4a8cff4cf2e7dd
Tom <redsoxfantom@gmail.com>
2015-01-12 20:10:23 -0500

    Added immediate messaging

--- GameUtilities/Services/MessageRouter/IMessageRouterService.cs
+++ GameUtilities/Services/MessageRouter/IMessageRouterService.cs
@@ -22,10 +22,19 @@ namespace GameUtilities.Services.MessageRouter
         void RegisterTopic(string Topic, IMessageDestination consumer);
 
         /// <summary>
-        /// Send a message to anyone subscribed to a Topic
+        /// Send a message to anyone subscribed to a Topic. The recepients will get it next frame
         /// </summary>
         /// <param name="Topic">The topic to associate this message with</param>
         /// <param name="message">The message to send</param>
         void SendMessage(string Topic, IMessage message);
+
+        /// <summary>
+        /// Send a message to one subscriber. The recepient will handle it immediately
+        /// </summary>
+        /// <param name="Topic">The topic to associate with this message</param>
+        /// <param name="message">The message to send</param>
+        /// <param name="ReturnValue">An object to return</param>
+        /// <returns>Whether or not the message successfully went through</returns>
+        bool SendMessageImmediate(string Topic, IMessage message, ref object ReturnValue);
     }
 }
