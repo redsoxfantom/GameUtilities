833aa2fc8ae27ca185aaa7a54475c4545fd472c3
Tom <redsoxfantom@gmail.com>
2015-01-12 20:00:50 -0500

    Added topic registration

---
+++ GameUtilities/Services/MessageRouter/IMessageRouterService.cs
@@ -0,0 +1,24 @@
+﻿using GameUtilities.Framework.Utilities.Message;
+using System.Collections.Generic;
+
+namespace GameUtilities.Services.MessageRouter
+{
+    /// <summary>
+    /// Interface for the message router
+    /// </summary>
+    public interface IMessageRouterService
+    {
+        /// <summary>
+        /// Get all messages for subscribed topics
+        /// </summary>
+        /// <returns>A dictionary of topics and list of messages associated with the topic</returns>
+        Dictionary<string,List<IMessage>> GetMessages();
+
+        /// <summary>
+        /// Register to receive Messages pertaining to a Topic
+        /// </summary>
+        /// <param name="Topic">The topic to subscribe for</param>
+        /// <param name="consumer">The object that is subscribing for this Topic</param>
+        void RegisterTopic(string Topic, IMessageDestination consumer);
+    }
+}
