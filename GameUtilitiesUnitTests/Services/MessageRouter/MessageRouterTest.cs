5c39e08b04c2cb800ea1e9fc994ef246258c1f2e
Tom <redsoxfantom@gmail.com>
2015-01-12 22:00:18 -0500

    Began work on unit test for message router

---
+++ GameUtilitiesUnitTests/Services/MessageRouter/MessageRouterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GameUtilities.Services.MessageRouter;
+using GameUtilities.Framework.Utilities.Message;
+
+namespace GameUtilitiesUnitTests.Services.MessageRouter
+{
+    /// <summary>
+    /// Test for the Message Router service
+    /// </summary>
+    [TestClass]
+    public class MessageRouterTest
+    {
+        /// <summary>
+        /// Test for message registration
+        /// </summary>
+        [TestMethod]
+        public void RegisterTopicTest()
+        {
+            MessageRouterService target = new MessageRouterService();
+            PrivateObject obj = new PrivateObject(target);
+        }
+    }
+}
