3b77423d0b3e8a75a03fd76e673020e581eebe83
Tom <redsoxfantom@gmail.com>
2015-01-15 17:58:12 -0500

    Added BaseConstants

---
+++ GameUtilities/Components/Constants/BaseConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace GameUtilities.Components.Constants
+{
+    /// <summary>
+    /// Basic constants
+    /// </summary>
+    public class BaseConstants
+    {
+        /// <summary>
+        /// X position
+        /// </summary>
+        public static readonly string POS_X = "PosX";
+
+        /// <summary>
+        /// Y position
+        /// </summary>
+        public static readonly string POS_Y = "PosY";
+
+        /// <summary>
+        /// Z position
+        /// </summary>
+        public static readonly string POS_Z = "PosZ";
+    }
+}
--- GameUtilities/Components/TestComponent.cs
+++ GameUtilities/Components/TestComponent.cs
@@ -9,9 +9,32 @@ namespace GameUtilities.Components
     /// </summary>
     public class TestComponent : BaseComponent
     {
+        /// <summary>
+        /// Initialize the component
+        /// </summary>
+        /// <param name="Context">The context</param>
+        /// <param name="data">entity data</param>
         public override void Init(IExecutableContext Context, DataSet data = null)
         {
             base.Init(Context, data);
+        }
+
+        /// <summary>
+        /// Update the component
+        /// </summary>
+        /// <param name="timeSinceLastFrame">How long since the last frame</param>
+        public override void Update(double timeSinceLastFrame)
+        {
+            base.Update(timeSinceLastFrame);
+        }
+
+        /// <summary>
+        /// Draw the square
+        /// </summary>
+        /// <param name="timeSinceLastFrame">How long since the last frame</param>
+        public override void Draw(double timeSinceLastFrame)
+        {
+            base.Draw(timeSinceLastFrame);
         }
     }
 }
