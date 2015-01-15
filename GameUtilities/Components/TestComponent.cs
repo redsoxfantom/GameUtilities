92d1b175ec5abfb30fa1ee564d7ddc39b69f88ed
Tom <redsoxfantom@gmail.com>
2015-01-15 17:53:35 -0500

    Added a test component

---
+++ GameUtilities/Components/TestComponent.cs
@@ -0,0 +1,17 @@
+﻿using GameUtilities.Framework.DataContracts;
+using GameUtilities.Framework.Utilities.ExecutableContext;
+using OpenTK;
+
+namespace GameUtilities.Components
+{
+    /// <summary>
+    /// Simple component for testing purposes
+    /// </summary>
+    public class TestComponent : BaseComponent
+    {
+        public override void Init(IExecutableContext Context, DataSet data = null)
+        {
+            base.Init(Context, data);
+        }
+    }
+}
