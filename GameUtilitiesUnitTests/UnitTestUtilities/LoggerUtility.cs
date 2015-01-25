777459106750e8a0396152d71e24d08fee0f41ab
Tom <redsoxfantom@gmail.com>
2015-01-24 21:10:02 -0500

    Added logger utility class for unit tests

---
+++ GameUtilitiesUnitTests/UnitTestUtilities/LoggerUtility.cs
@@ -0,0 +1,12 @@
+﻿using GameUtilities.Framework.Utilities.Loggers;
+
+namespace GameUtilitiesUnitTests.UnitTestUtilities
+{
+    /// <summary>
+    /// Logger utility class for use with Unit tests
+    /// </summary>
+    public class LoggerUtility : ILogger
+    {
+
+    }
+}
