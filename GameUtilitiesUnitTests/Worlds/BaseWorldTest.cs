a53af5b5b78c1d9bf831e9a4cdcb2d2779505249
Tom <redsoxfantom@gmail.com>
2015-01-26 21:23:04 -0500

    Fixed WorldInitTest

--- GameUtilitiesUnitTests/Worlds/BaseWorldTest.cs
+++ GameUtilitiesUnitTests/Worlds/BaseWorldTest.cs
@@ -9,6 +9,7 @@ using GameUtilities.Entities.DataContracts;
 using GameUtilitiesUnitTests.UnitTestUtilities;
 using GameUtilities.Framework.Utilities.ExecutableContext;
 using GameUtilities.Framework.Utilities.Message.MessageDispatch;
+using GameUtilities.Framework.Utilities.FilePathResolver;
 
 namespace GameUtilitiesUnitTests.Worlds
 {
@@ -107,14 +108,27 @@ namespace GameUtilitiesUnitTests.Worlds
         [TestMethod]
         public void WorldInitSuccessTest()
         {
+            List<EntityData> entities = new List<EntityData>();
+            entities.Add(new EntityData("TestEntityType", "GameUtilities.Entities.BaseEntity,GameUtilities"));
+            WorldData data = new WorldData("TEST", "TEST", entities);
+            target = new BaseWorld(data);
+            obj = new PrivateObject(target);
+            logger = new LoggerUtility("logger");
+            obj.SetFieldOrProperty("mLogger", logger);
             Mock<IExecutableContext> contextMock = new Mock<IExecutableContext>();
             Mock<IMessageRouter> routerMock = new Mock<IMessageRouter>();
+            ConfigManager config = new ConfigManager();
+            config.Init(".\\Config\\");
             contextMock.Setup(f => f.MessageRouter).Returns(routerMock.Object);
+            contextMock.Setup(f => f.ConfigManager).Returns(config);
 
             target.Init(contextMock.Object);
 
-            routerMock.Verify(f => f.RegisterTopic("TEST", target));
             Assert.IsTrue(logger.ErrorMessages.Count == 0);
+            List<IEntity> entList = (List<IEntity>)obj.GetFieldOrProperty("EntityList");
+            Dictionary<string, IEntity> EntityIdDictionary = (Dictionary<string, IEntity>)obj.GetFieldOrProperty("EntityIdDictionary");
+            Assert.AreEqual(1, entList.Count);
+            Assert.AreEqual(1, EntityIdDictionary.Keys.Count);
         }
     }
 }
