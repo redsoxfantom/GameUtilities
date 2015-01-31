d6d6c60a78bce6152d180b29411a932ab0534d07
Tom <redsoxfantom@gmail.com>
2015-01-31 13:24:10 -0500

    Created a List of services that the Engine will load

---
+++ GameUtilities/Services/DataContracts/ServiceDataContract.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GameUtilities.Services.DataContracts
+{
+    /// <summary>
+    /// Data Contract defining a list of services that the Engine will use
+    /// </summary>
+    [DataContract(Name = "ServicesList", Namespace = "http://schemas.microsoft.com/2003/10/Serialization/Arrays")]
+    public class ServiceDataContract : List<string>
+    {
+    }
+}
