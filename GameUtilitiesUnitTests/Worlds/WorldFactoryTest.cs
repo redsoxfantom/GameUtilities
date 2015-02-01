f11e873b363cea8a8ec3a2bfb06dab8ff4f3d9db
Tom <redsoxfantom@gmail.com>
2015-02-01 17:19:48 -0500

    Created WorldFactoryTest

---
+++ GameUtilitiesUnitTests/Worlds/WorldFactoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GameUtilities.Worlds.DataContracts;
+using GameUtilities.Framework.Utilities.ExecutableContext;
+using GameUtilities.Framework.Utilities.FilePathResolver;
+using System.IO;
+
+namespace GameUtilitiesUnitTests.Worlds
+{
+    /// <summary>
+    /// Test of the WorldFactory class
+    /// </summary>
+    [TestClass]
+    public class WorldFactoryTest
+    {
+        /// <summary>
+        /// Test the create world method with a world that doesn't exist
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(DirectoryNotFoundException))]
+        public void CreateWorldNoWorld()
+        {
+            Mock<IExecutableContext> execContextMock = new Mock<IExecutableContext>();
+            ConfigManager config = new ConfigManager();
+            config.Init(".\\TestConfig");
+            execContextMock.Setup(f => f.ConfigManager).Returns(config);
+
+            WorldFactory.CreateWorld("FakeName", execContextMock.Object);
+        }
+    }
+}
