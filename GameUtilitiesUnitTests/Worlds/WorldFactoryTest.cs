488f466530f0fe6ae28777f6028ef72e5119a174
Tom <redsoxfantom@gmail.com>
2015-02-02 18:54:14 -0500

    Added test for World factory when given a valid world

--- GameUtilitiesUnitTests/Worlds/WorldFactoryTest.cs
+++ GameUtilitiesUnitTests/Worlds/WorldFactoryTest.cs
@@ -5,6 +5,11 @@ using GameUtilities.Worlds.DataContracts;
 using GameUtilities.Framework.Utilities.ExecutableContext;
 using GameUtilities.Framework.Utilities.FilePathResolver;
 using System.IO;
+using GameUtilities.Worlds;
+using System.Collections.Generic;
+using GameUtilities.Entities;
+using GameUtilitiesUnitTests.UnitTestUtilities;
+using GameUtilities.Framework.Utilities.Message.MessageDispatch;
 
 namespace GameUtilitiesUnitTests.Worlds
 {
@@ -18,8 +23,8 @@ namespace GameUtilitiesUnitTests.Worlds
         /// Test the create world method with a world that doesn't exist
         /// </summary>
         [TestMethod]
-        [ExpectedException(typeof(DirectoryNotFoundException))]
-        public void CreateWorldNoWorld()
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void CreateWorldInvalid()
         {
             Mock<IExecutableContext> execContextMock = new Mock<IExecutableContext>();
             ConfigManager config = new ConfigManager();
@@ -27,6 +32,28 @@ namespace GameUtilitiesUnitTests.Worlds
             execContextMock.Setup(f => f.ConfigManager).Returns(config);
 
             WorldFactory.CreateWorld("FakeName", execContextMock.Object);
+        }
+
+        /// <summary>
+        /// Test the create world method with a world that exists
+        /// </summary>
+        [TestMethod]
+        public void CreateWorldValid()
+        {
+            Mock<IExecutableContext> execContextMock = new Mock<IExecutableContext>();
+            Mock<IMessageRouter> msgRouterMock = new Mock<IMessageRouter>();
+            ConfigManager config = new ConfigManager();
+            config.Init(".\\TestConfig");
+            execContextMock.Setup(f => f.ConfigManager).Returns(config);
+            execContextMock.Setup(f => f.MessageRouter).Returns(msgRouterMock.Object);
+
+            BaseWorld world = (BaseWorld)WorldFactory.CreateWorld("World1", execContextMock.Object);
+
+            PrivateObject obj = new PrivateObject(world);
+            List<IEntity> EntityList = (List<IEntity>)obj.GetFieldOrProperty("EntityList");
+            Assert.IsTrue(EntityList.Count == 2);
+            Assert.IsTrue(EntityList[0].GetType() == typeof(BaseEntity));
+            Assert.IsTrue(EntityList[1].GetType() == typeof(BaseEntity));
         }
     }
 }
