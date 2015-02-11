307311e9cc4e068fb131a17b75f0a79c75abe6b9
Tom <redsoxfantom@gmail.com>
2015-02-11 17:53:41 -0500

    Added constructor to EngineConfig data contract

--- GameUtilities/Framework/Engine/DataContracts/EngineConfigDataContract.cs
+++ GameUtilities/Framework/Engine/DataContracts/EngineConfigDataContract.cs
@@ -14,5 +14,14 @@ namespace GameUtilities.Framework.Engine.DataContracts
         /// </summary>
         [DataMember(IsRequired=true,Order=0)]
         public ServiceDataContract Services { get; private set;}
+
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="mServices">A list of services that this engine supports</param>
+        public EngineConfigDataContract(ServiceDataContract mServices)
+        {
+            Services = mServices;
+        }
     }
 }
