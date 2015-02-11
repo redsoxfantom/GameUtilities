3f5514767eaf8039805e229a5a110d01748fe0e9
Tom <redsoxfantom@gmail.com>
2015-02-11 17:52:20 -0500

    Created EngineConfig data contract

---
+++ GameUtilities/Framework/Engine/DataContracts/EngineConfigDataContract.cs
@@ -0,0 +1,18 @@
+﻿using GameUtilities.Services.DataContracts;
+using System.Runtime.Serialization;
+
+namespace GameUtilities.Framework.Engine.DataContracts
+{
+    /// <summary>
+    /// Data contract for Engine Config files 
+    /// </summary>
+    [DataContract(Name="EngineConfig",Namespace="")]
+    public class EngineConfigDataContract
+    {
+        /// <summary>
+        /// The list of Services that this Engine supports
+        /// </summary>
+        [DataMember(IsRequired=true,Order=0)]
+        public ServiceDataContract Services { get; private set;}
+    }
+}
