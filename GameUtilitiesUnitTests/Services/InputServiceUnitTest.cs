167ee78d21382f64a3810972734903835b023e8c
Tom <redsoxfantom@gmail.com>
2015-02-19 17:45:33 -0500

    Created InputService test, added Initialize method

---
+++ GameUtilitiesUnitTests/Services/InputServiceUnitTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameUtilities.Services;
+using GameUtilitiesUnitTests.UnitTestUtilities;
+using GameUtilities.Framework.Utilities.Message.MessageDispatch;
+using Moq;
+using GameUtilities.Framework.Utilities.ExecutableContext;
+
+namespace GameUtilitiesUnitTests.Services
+{
+    /// <summary>
+    /// Test class for the InputService class
+    /// </summary>
+    [TestClass]
+    public class InputServiceUnitTest
+    {
+        /// <summary>
+        /// The class under test
+        /// </summary>
+        private InputService target;
+
+        /// <summary>
+        /// Accessor to private methods/fields
+        /// </summary>
+        private PrivateObject po;
+
+        /// <summary>
+        /// The logger
+        /// </summary>
+        private LoggerUtility logger;
+
+        /// <summary>
+        /// Mock up of the message router
+        /// </summary>
+        private Mock<IMessageRouter> msgRouterMock;
+
+        /// <summary>
+        /// Mock up of the executable context
+        /// </summary>
+        private Mock<IExecutableContext> execContextMock;
+
+        /// <summary>
+        /// Handles first time setup of the test
+        /// </summary>
+        [TestInitialize]
+        public void Initializze()
+        {
+            target = new InputService();
+            po = new PrivateObject(target);
+            logger = new LoggerUtility("test");
+            msgRouterMock = new Mock<IMessageRouter>();
+            execContextMock = new Mock<IExecutableContext>();
+            execContextMock.Setup(f => f.MessageRouter).Returns(msgRouterMock.Object);
+            po.SetFieldOrProperty("mLogger", logger);
+            target.Init(execContextMock.Object);
+        }
+    }
+}
