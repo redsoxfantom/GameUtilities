1a26ad5810c918034cfdca3c9148f351a6caf905
Tom <redsoxfantom@gmail.com>
2015-02-19 17:49:13 -0500

    Created testkeyboardState

---
+++ GameUtilitiesUnitTests/UnitTestUtilities/TestKeyBoardState.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK.Input;
+
+namespace GameUtilitiesUnitTests.UnitTestUtilities
+{
+    /// <summary>
+    /// Class used for mocking out Keypresses
+    /// </summary>
+    public class TestKeyBoardState : KeyboardState
+    {
+    }
+}
