a832db070747ddde696ee1397c50f81a196e781e
Tom <redsoxfantom@gmail.com>
2015-02-19 18:13:53 -0500

    Created IInputHandler

---
+++ GameUtilities/Framework/Utilities/InputHandlers/IInputHandler.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Input;
+
+namespace GameUtilities.Framework.Utilities.InputHandlers
+{
+    public interface IInputHandler
+    {
+        /// <summary>
+        /// Read in all connected inputs
+        /// </summary>
+        void GetInputs();
+
+        /// <summary>
+        /// Check if a key is currently pressed
+        /// </summary>
+        /// <param name="key">key to check</param>
+        /// <returns>whether or not the key is pressed</returns>
+        bool isKeyDown(Key key);
+    }
+}
