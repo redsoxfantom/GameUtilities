98000eba747ef4fc98c215a768bcf0d34640d663
Tom <redsoxfantom@gmail.com>
2015-05-30 20:59:03 -0400

    Created base class for threaded services

---
+++ GameUtilities/Services/ThreadedService/BaseThreadedService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameUtilities.Services;
+using GameUtilities.Framework.Utilities.Message;
+
+namespace GameUtilities.Services.ThreadedService
+{
+    /// <summary>
+    /// Base class for Services that will spawn a thread to do work
+    /// </summary>
+    public abstract class BaseThreadedService : BaseService
+    {
+        /// <summary>
+        /// Handle messages sent since last frame
+        /// </summary>
+        /// <param name="timeSinceLastFrame">How much time has elapsed since last frame</param>
+        /// <param name="messages">Messages received</param>
+        public override void Update(double timeSinceLastFrame, Dictionary<string, List<IMessage>> messages)
+        {
+            object retObj = new object();
+
+            foreach (string Topic in messages.Keys)
+            {
+                foreach (IMessage message in messages[Topic])
+                {
+                    HandleMessage(Topic, message, ref retObj);
+                }
+            }
+        }
+    }
+}
